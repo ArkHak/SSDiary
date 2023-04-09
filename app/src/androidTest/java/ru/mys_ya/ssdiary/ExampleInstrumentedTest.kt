273b4de08991e39792e7820eb608758edfdb6a82
Oleg Mysin <7nacu7@gmail.com>
2023-04-09 23:46:45 +0300

    Initial commit

---
+++ app/src/androidTest/java/ru/mys_ya/ssdiary/ExampleInstrumentedTest.kt
@@ -0,0 +1,19 @@
+package ru.mys_ya.ssdiary
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("ru.mys_ya.ssdiary", appContext.packageName)
+    }
+}
