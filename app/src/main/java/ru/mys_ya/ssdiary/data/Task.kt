40173f02f8cb2e54278a58538be64d3921d159fd
Oleg Mysin <7nacu7@gmail.com>
2023-04-12 23:19:52 +0300

    v0.1.1     - refactor ItemTask;

--- app/src/main/java/ru/mys_ya/ssdiary/data/Task.kt
+++ app/src/main/java/ru/mys_ya/ssdiary/data/Task.kt
@@ -7,3 +7,11 @@ data class Task(
     val name: String,
     val description: String,
 )
+
+val simpleTask = Task(
+    id = 1,
+    dateStart = 1681318800,
+    dateEnd = 1681322400,
+    name = "Simple Task",
+    description = "Description Simple Task"
+)
--- app/src/main/java/ru/mys_ya/ssdiary/ui/screens/HomeScreen.kt
+++ app/src/main/java/ru/mys_ya/ssdiary/ui/screens/HomeScreen.kt
@@ -2,15 +2,24 @@ package ru.mys_ya.ssdiary.ui.screens
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import io.github.boguszpawlowski.composecalendar.SelectableWeekCalendar
 import io.github.boguszpawlowski.composecalendar.WeekCalendarState
 import io.github.boguszpawlowski.composecalendar.rememberSelectableWeekCalendarState
 import io.github.boguszpawlowski.composecalendar.selection.DynamicSelectionState
 import ru.mys_ya.ssdiary.R
+import ru.mys_ya.ssdiary.data.Task
+import ru.mys_ya.ssdiary.data.simpleTask
 
 @Composable
 fun HomeScreen(
@@ -23,9 +32,11 @@ fun HomeScreen(
         modifier = modifier.padding(dimensionResource(id = R.dimen.default_padding))
     ) {
         Calendar(calendarState = calendarState)
-        ItemTask(
-            id = calendarState.selectionState.selection.joinToString { it.toString() },
-            onClickItem = { onSelectTask(it) }
+        TaskList(
+            task = simpleTask,
+            onSelectTask = {
+                onSelectTask(it)
+            }
         )
     }
 }
@@ -39,22 +50,47 @@ fun Calendar(
 }
 
 @Composable
+private fun TaskList(
+    modifier: Modifier = Modifier,
+    task: Task,
+    onSelectTask: (String) -> Unit,
+) {
+    LazyColumn(
+        modifier = modifier.fillMaxWidth(),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        contentPadding = PaddingValues(16.dp)
+    ) {
+        item {
+            ItemTask(
+                task = task,
+                onClickItem = { onSelectTask(it) }
+            )
+        }
+    }
+}
+
+@Composable
 fun ItemTask(
-    id: String,
+    task: Task,
     modifier: Modifier = Modifier,
     onClickItem: (String) -> Unit,
 ) {
-    Column(
-        modifier = modifier.fillMaxWidth()
-    ) {
-        Box(modifier = modifier
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
             .clickable {
-                onClickItem(id)
-            }) {
-            Column {
-                Text(text = "Название")
-                Text(text = id)
-            }
+                onClickItem(task.name)
+            },
+        elevation = 8.dp,
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Column(
+            modifier = modifier.padding(dimensionResource(id = R.dimen.default_padding)),
+            horizontalAlignment = Alignment.Start,
+        ) {
+            Text(text = task.name, fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            Spacer(modifier = modifier.height(2.dp))
+            Text(text = task.dateStart.toString(), fontSize = 14.sp)
         }
     }
 }
